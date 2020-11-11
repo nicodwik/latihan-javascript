4130d9a83b9b6a605fd73a042441fdc297e7cae9
nicodwik <nicodwika@gmail.com>
2020-11-11 19:47:21 +0700

    pengumpulan tugas array + object

---
+++ soal1.js
@@ -0,0 +1,4 @@
+var daftarBuah = ["2. Apel", "5. Jeruk", "3. Anggur", "4. Semangka", "1. Mangga"]
+
+var sort = daftarBuah.sort()
+console.log(sort)
---
+++ soal2.js
@@ -0,0 +1,3 @@
+var kalimat="saya sangat senang belajar javascript"
+var split = kalimat.split(' ')
+console.log(split)
